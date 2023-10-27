af723e7e57ddde555cb91f3ae554af95a652ed65
Vinod Allam <vinod.allam74@gmail.com>
2023-10-27 22:30:05 +0530

    virtual coin toss

--- coin_toss.py
+++ coin_toss.py
@@ -1,7 +1,6 @@
 # Write your code below this line 👇
 # Hint: Remember to import the random module first. 🎲
 import random
-
 random_number = random.randint(0, 1)
 if random_number == 0:
     print("Tails")
